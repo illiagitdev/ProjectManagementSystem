039d3aaf285d27e8f98bbf0203b9fe9f18731715
illia_dev <illya.posv.b@gmail.com>
2020-02-28 18:20:50 +0200

    add Developer and DeveloperDAO classes

---
+++ src/main/java/com/homework/goit/developer/Developer.java
@@ -0,0 +1,11 @@
+package com.homework.goit.developer;
+
+import com.homework.goit.common.Entity;
+
+public class Developer extends Entity {
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String sex;
+
+}
---
+++ src/main/java/com/homework/goit/developer/DeveloperDAO.java
@@ -0,0 +1,32 @@
+package com.homework.goit.developer;
+
+import com.homework.goit.common.DataAccessObject;
+
+import java.util.List;
+
+public class DeveloperDAO extends DataAccessObject<Developer> {
+    @Override
+    public void create(Developer developer) {
+
+    }
+
+    @Override
+    public void update(Developer developer) {
+
+    }
+
+    @Override
+    public Developer getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Developer> getAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
