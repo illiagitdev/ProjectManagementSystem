2b185d54fee39814d7fc5387d2419824e86755ce
illiagitdev <illya.posv.b@gmail.com>
2020-02-29 10:10:37 +0200

    update DAO abstraction

--- src/main/java/com/homework/goit/common/DataAccessObject.java
+++ src/main/java/com/homework/goit/common/DataAccessObject.java
@@ -1,8 +1,15 @@
 package com.homework.goit.common;
 
+import java.sql.Connection;
 import java.util.List;
 
 public abstract class DataAccessObject<T extends Entity> {
+        protected Connection connection;
+
+        public DataAccessObject(Connection connection) {
+                this.connection = connection;
+        }
+
         public abstract void create(T t);
         public abstract void update(T t);
         public abstract T getById(int id);
